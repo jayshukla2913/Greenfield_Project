5ed500fe4784d634cb5a0fd9c3059c53c5d583d3
Jayprakash Shukla <jayshukla29.13@gmail.com>
2026-03-14 18:43:10 +0530

    updated multiple terraform files

--- terraform-infra/modules/ecs/outputs.tf
+++ terraform-infra/modules/ecs/outputs.tf
@@ -5,3 +5,8 @@ output "cluster_name" {
 output "service_name" {
   value = aws_ecs_service.service.name
 }
+
+variable "private_subnets" {
+  description = "Private subnets for ECS service"
+  type        = list(string)
+}
