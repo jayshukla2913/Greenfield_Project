4c0b0184b829f820e33308cd8db4a9343f3679d2
Jayprakash Shukla <jayshukla29.13@gmail.com>
2026-03-14 16:23:32 +0530

    updated security groups for rds

--- terraform-infra/modules/security/main.tf
+++ terraform-infra/modules/security/main.tf
@@ -54,7 +54,7 @@ resource "aws_security_group" "rds_sg" {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    security_groups = [var.ecs_security_group]
+    security_groups = [aws_security_group.app_sg.id]
 
   }
 
--- terraform-infra/modules/security/variables.tf
+++ terraform-infra/modules/security/variables.tf
@@ -2,8 +2,3 @@ variable "vpc_id" {
   description = "VPC ID for ALB"
   type        = string
 }
-
-variable "ecs_security_group" {
-  description = "ECS security group ID"
-  type        = string
-}
