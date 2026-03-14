ff727415cdbb5ac8443ae45b015753e031ca66dd
Jayprakash Shukla <jayshukla29.13@gmail.com>
2026-03-14 19:17:31 +0530

    updated multiple terraform files

--- terraform-infra/modules/alb/main.tf
+++ terraform-infra/modules/alb/main.tf
@@ -5,9 +5,18 @@ resource "aws_lb" "alb" {
 }
 
 resource "aws_lb_target_group" "tg" {
-  port     = var.container_port
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name        = "greenfield-tg"
+  port        = var.container_port
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_lb_listener" "listener" {
