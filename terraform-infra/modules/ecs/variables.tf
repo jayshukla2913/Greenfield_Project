9095fd53d76ff038416d72e96b0b2d461fb0ed3f
Jayprakash Shukla <jayshukla29.13@gmail.com>
2026-03-14 20:01:02 +0530

    updated multiple terraform files

--- terraform-infra/modules/ecs/variables.tf
+++ terraform-infra/modules/ecs/variables.tf
@@ -2,3 +2,8 @@ variable "cluster_name" {}
 variable "target_group_arn" {}
 variable "security_group" {}
 variable "container_port" {}
+
+variable "public_subnets" {
+  description = "Private subnets for ECS service"
+  type        = list(string)
+}
