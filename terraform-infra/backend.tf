7263d55b7ee587465be4f9ef19a93364c859c395
Jayprakash Shukla <jayshukla29.13@gmail.com>
2026-03-14 14:28:21 +0530

    updated backend for terraform state file

--- terraform-infra/backend.tf
+++ terraform-infra/backend.tf
@@ -5,7 +5,7 @@ terraform {
     bucket         = "greenfield-terraform-state-bucket"
     key            = "greenfield-project/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
+    use_lockfile   = true
     encrypt        = true
   }
 
