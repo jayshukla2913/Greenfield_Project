cdbbb3c4b2d00617fbdb2a4e7755a5de81233d88
Jayprakash Shukla <jayshukla29.13@gmail.com>
2026-03-14 17:57:37 +0530

    updated security groups for rds

--- terraform-infra/modules/alb/variables.tf
+++ terraform-infra/modules/alb/variables.tf
@@ -1,19 +1,4 @@
-variable "vpc_id" {
-  description = "VPC ID for ALB"
-  type        = string
-}
-
-variable "public_subnets" {
-  description = "Public subnets for ALB"
-  type        = list(string)
-}
-
-variable "alb_sg" {
-  description = "Security group for ALB"
-  type        = string
-}
-
-variable "container_port" {
-  description = "Container port"
-  type        = number
-}
+variable "vpc_id" {}
+variable "public_subnets" {}
+variable "alb_sg" {}
+variable "container_port" {}
--- terraform-infra/modules/rds/main.tf
+++ terraform-infra/modules/rds/main.tf
@@ -1,11 +1,13 @@
 resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "greenfield-db-subnet-group"
   subnet_ids = var.private_subnets
 }
 
 resource "aws_db_instance" "mysql" {
-  engine         = "mysql"
-  instance_class = "db.t3.micro"
+  identifier = "greenfield-mysql"
 
+  engine            = "mysql"
+  instance_class    = "db.t3.micro"
   allocated_storage = 20
 
   username = var.db_username
@@ -14,7 +16,7 @@ resource "aws_db_instance" "mysql" {
   db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
 
   vpc_security_group_ids = [
-    var.ecs_security_group
+    var.security_group
   ]
 
   skip_final_snapshot = true
