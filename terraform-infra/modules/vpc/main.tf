f48c5e36ca90a93b6eec8edbbca8268d56a15633
Jayprakash Shukla <jayshukla29.13@gmail.com>
2026-03-14 18:40:01 +0530

    updated multiple terraform files

--- terraform-infra/modules/vpc/main.tf
+++ terraform-infra/modules/vpc/main.tf
@@ -10,18 +10,30 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_subnet" "public" {
-  count = length(var.public_subnets)
-
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnets[count.index]
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "private" {
-  count = length(var.private_subnets)
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
 
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnets[count.index]
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
 }
 
 resource "aws_db_subnet_group" "db_subnet_group" {
--- terraform-infra/modules/vpc/outputs.tf
+++ terraform-infra/modules/vpc/outputs.tf
@@ -3,9 +3,15 @@ output "vpc_id" {
 }
 
 output "public_subnets" {
-  value = aws_subnet.public[*].id
+  value = [
+    aws_subnet.public1.id,
+    aws_subnet.public2.id
+  ]
 }
 
 output "private_subnets" {
-  value = aws_subnet.private[*].id
+  value = [
+    aws_subnet.private1.id,
+    aws_subnet.private2.id
+  ]
 }
